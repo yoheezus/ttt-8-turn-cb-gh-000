1469e1b4679a9f596fce9a1b07c38149cf862b8f
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-01 21:38:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,8 @@ def turn(board)
     if valid_move?(board, converted_input) == true
         move(board, converted_input)
     else
-        puts "test"
+        puts "not a va"
+        new_input = gets.strip()
     end
     display_board(board)
 end
