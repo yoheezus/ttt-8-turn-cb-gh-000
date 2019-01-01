74fed05600026dfe0d5558748217a022ae666be4
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-01 21:51:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,7 +22,7 @@ def valid_move?(board, index)
         false
     elsif position_taken?(board, index) == false
         true
-    elsif index.between?(0, 9)
+    elsif index.between?(0, 9) == false
         nil
     else
         nil
